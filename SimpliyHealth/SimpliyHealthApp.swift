eb485934e998fe6c566c044ccb2c3d5b30fdf3ab
Kevin Chong <chongkev@gmail.com>
2025-11-01 10:43:38 +0800

    Initial commit

---
+++ SimpliyHealth/SimpliyHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpliyHealthApp.swift
+//  SimpliyHealth
+//
+//  Created by Kevin Chong on 1/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpliyHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpliyHealthTests/SimpliyHealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpliyHealthTests.swift
+//  SimpliyHealthTests
+//
+//  Created by Kevin Chong on 1/11/2025.
+//
+
+import Testing
+@testable import SimpliyHealth
+
+struct SimpliyHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
