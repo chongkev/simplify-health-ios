b0050a409e36a1e76192eaf6592ca448a0a87926
Kevin Chong <chongkev@gmail.com>
2025-11-10 21:22:41 +0800

    Add VideosView

--- SimplifyHealth/MainView.swift
+++ SimplifyHealth/MainView.swift
@@ -25,23 +25,14 @@ struct MainView: View {
     ]
     
     // Sample data for the tiles
-    let items = [
-        Item(name: "Pysiotherapy", iconSystemName: "figure.rolling", color: Color(red: 75/255.0, green: 125/255.0, blue: 116/255.0)),
-        Item(name: "Occupational Therapy", iconSystemName: "figure.walk", color: Color(red: 116/255.0, green: 78/255.0, blue: 32/255.0)),
-        Item(name: "Dietetics", iconSystemName: "frying.pan", color: Color(red: 90/255.0, green: 144/255.0, blue: 200/255.0)),
-        Item(name: "Speech Pathlogy", iconSystemName: "voiceover", color: Color(red: 199/255.0, green: 105/255.0, blue: 106/255.0)),
-        Item(name: "Fails Prevention", iconSystemName: "figure.fall", color: Color(red: 236/255.0, green: 223/255.0, blue: 205/255.0)),
-        Item(name: "Home Safety Audit", iconSystemName: "house.lodge", color: Color(red: 84/255.0, green: 70/255.0, blue: 135/255.0)),
-        Item(name: "Telehealth How-To", iconSystemName: "stethoscope", color: Color(red: 253/255.0, green: 159/255.0, blue: 40/255.0)),
-        Item(name: "Contact Us", iconSystemName: "bubble.left.and.bubble.right", color: Color(red: 11/255.0, green: 38/255.0, blue: 53/255.0)),
-    ]
+    let items: [ItemType] = Dummy.mainViewItems
     
     init() {
         UIScrollView.appearance().bounces = false
     }
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             GeometryReader { geometry in
                 ScrollView {
                     Text("Simplify Health")
@@ -52,12 +43,15 @@ struct MainView: View {
                         .padding(.bottom, 28)
 
                     LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(items, id: \.name) { item in
-                            TileView(
-                                title: item.name,
-                                iconSystemName: item.iconSystemName,
-                                color: item.color,
-                                height: isLandscape(geometry) ? 280 : 300)
+                        ForEach(items, id: \.title) { item in
+                            NavigationLink(destination: detailView(for: item)) {
+                                TileView(
+                                    title: item.title,
+                                    iconSystemName: item.iconSystemName,
+                                    color: item.color,
+                                    height: isLandscape(geometry) ? 280 : 300
+                                )
+                            }
                         }
                     }
                 }
@@ -70,7 +64,54 @@ struct MainView: View {
         .navigationViewStyle(.stack)
     }
     
+    @ViewBuilder
+    func detailView(for item: ItemType) -> some View {
+        switch item {
+        case let .contentCategory(title, _, color, videoItems):
+            VideosView(
+                viewModel: .init(
+                    title: title,
+                    items: videoItems,
+                    backgroundColor: color
+                )
+            )
+        case .contactUs:
+            ScrollView { Text("CONTACT US") }
+        }
+    }
+    
     func isLandscape(_ geometry: GeometryProxy) -> Bool { geometry.size.width > geometry.size.height }
+}
+
+extension MainView {
+    enum ItemType {
+        case contentCategory(
+            title: String,
+            iconSystemName: String,
+            color: Color,
+            videoItems: [VideoItem]
+        )
+        case contactUs
+        
+        var title: String {
+            switch self {
+            case let .contentCategory(title, _, _, _): title
+            case .contactUs: "Contact Us"
+            }
+        }
+        var iconSystemName: String {
+            switch self {
+            case let .contentCategory(_, iconSystemName, _, _): iconSystemName
+            case .contactUs: "bubble.left.and.bubble.right"
+            }
+        }
+        var color: Color {
+            switch self {
+            case let .contentCategory(_, _, color, _): color
+            case .contactUs: Color(red: 11/255.0, green: 38/255.0, blue: 53/255.0)
+            }
+        }
+    }
 }
 
 struct Item {
@@ -86,7 +127,8 @@ struct TileView: View {
     let height: CGFloat
     
     var body: some View {
-        Button(action: { print("Tapped \(title)") }) {
+//        Button(action: { print("Tapped \(title)") }) {
+        Group {
             VStack {
                 Image(systemName: iconSystemName)
                     .font(.system(size: 80, weight: .bold))
@@ -102,7 +144,8 @@ struct TileView: View {
             .frame(height: height) // Give the tile a fixed height
             .frame(maxWidth: .infinity) // Important for flexible width
         }
-        .buttonStyle(TappableStyle(color: color))
+//        .buttonStyle(TappableStyle(color: color))
+        .background(color)
         .contentShape(Rectangle()) // Ensures the whole tile is tappable
         .cornerRadius(10)
     }
@@ -128,6 +171,72 @@ extension Color {
         uiColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
         return brightness < 0.8
     }
+    
+    /// Darkens the color by a specified percentage (0.0 to 1.0)
+    func brighten(by percentage: CGFloat = 0.2) -> Color {
+        let uiColor = UIColor(self)
+        var hue: CGFloat = 0
+        var saturation: CGFloat = 0
+        var brightness: CGFloat = 0
+        var alpha: CGFloat = 0
+        uiColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+        
+        // Adjust the brightness, clamping the value between 0 and 1
+        let newBrightness = max(0, min(1, brightness + percentage))
+        
+        // Create a new Color from the adjusted HSB values
+        return Color(uiColor: UIColor(hue: hue, saturation: saturation, brightness: newBrightness, alpha: alpha))
+    }
+}
+
+extension Dummy {
+    static let mainViewItems: [MainView.ItemType] = {
+        [
+            .contentCategory(
+                title: "Pysiotherapy",
+                iconSystemName: "figure.rolling",
+                color: Color(red: 75/255.0, green: 125/255.0, blue: 116/255.0),
+                videoItems: Dummy.physiotherapyVideoItems
+            ),
+            .contentCategory(
+                title: "Occupational Therapy",
+                iconSystemName: "figure.walk",
+                color: Color(red: 116/255.0, green: 78/255.0, blue: 32/255.0),
+                videoItems: Dummy.occupationalTherapyVideoItems
+            ),
+            .contentCategory(
+                title: "Dietetics",
+                iconSystemName: "frying.pan",
+                color: Color(red: 90/255.0, green: 144/255.0, blue: 200/255.0),
+                videoItems: Dummy.dieteticsVideoItems
+            ),
+            .contentCategory(
+                title: "Speech Pathlogy",
+                iconSystemName: "voiceover",
+                color: Color(red: 199/255.0, green: 105/255.0, blue: 106/255.0),
+                videoItems: Dummy.speechPathologyVideoItems
+            ),
+            .contentCategory(
+                title: "Falls Prevention",
+                iconSystemName: "figure.fall",
+                color: Color(red: 236/255.0, green: 223/255.0, blue: 205/255.0),
+                videoItems: Dummy.fallsPreventionVideoItems
+            ),
+            .contentCategory(
+                title: "Home Safety Audit",
+                iconSystemName: "house.lodge",
+                color: Color(red: 84/255.0, green: 70/255.0, blue: 135/255.0),
+                videoItems: Dummy.homeSafetyAuditVideoItems
+            ),
+            .contentCategory(
+                title: "Telehealth How-To",
+                iconSystemName: "stethoscope",
+                color: Color(red: 253/255.0, green: 159/255.0, blue: 40/255.0),
+                videoItems: Dummy.telehealthHowToVideoItems
+            ),
+            .contactUs,
+        ]
+    }()
 }
 
 #Preview {
---
+++ SimplifyHealth/VideosView.swift
@@ -0,0 +1,150 @@
+//
+//  VideosView.swift
+//  SimplifyHealth
+//
+//  Created by Kevin Chong on 2/11/2025.
+//
+
+import SwiftUI
+import Combine
+
+struct VideosView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @ObservedObject var viewModel: ViewModel
+
+    var columns: [GridItem] {
+//        Array(repeating: .init(.flexible()), count: horizontalSizeClass == .compact ? 1 : 2)
+        [.init(.flexible())]
+    }
+
+    var body: some View {
+        ScrollView {
+            Text(viewModel.title)
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(viewModel.backgroundColor.useWhiteText ? .white : .black.opacity(0.75))
+                .opacity(0.75)
+                .padding(.bottom, 28)
+
+            LazyVGrid(columns: columns, spacing: 24) {
+                ForEach(viewModel.items, id: \.title) { item in
+                    VideoItemView(
+                        title: item.title,
+                        description: item.description,
+                        thumbnailURL: item.thumbnailURL,
+                        backgroundColor: viewModel.backgroundColor.brighten(by: -0.2)
+                    )
+                    .padding(.horizontal, 24)
+//                    .padding(.bottom, 24)
+                }
+            }
+        }
+        .background(viewModel.backgroundColor.ignoresSafeArea())
+    }
+}
+
+struct VideoItemView: View {
+    let title: String
+    let description: String
+    let thumbnailURL: String
+    let backgroundColor: Color
+    
+    var body: some View {
+        Button(action: { print("Video tapped") }) {
+            HStack(alignment: .top) {
+                RemoteImageView(imageURL: URL(string: thumbnailURL)!)
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(10)
+                    .padding(.trailing, 16)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(title)
+                        .multilineTextAlignment(.leading)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(backgroundColor.useWhiteText ? .white : .black.opacity(0.75))
+                    
+                    Text(description)
+                        .lineLimit(5)
+                        .multilineTextAlignment(.leading)
+                        .font(.title)
+                        .foregroundColor(backgroundColor.useWhiteText ? .white : .black.opacity(0.75))
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(maxWidth: .infinity)
+            .padding(12)
+        }
+        .buttonStyle(TappableStyle(color: backgroundColor))
+        .contentShape(Rectangle())
+        .cornerRadius(10)
+    }
+}
+
+struct VideoItem {
+    let title: String
+    let description: String
+    let url: String
+    let thumbnailURL: String
+}
+
+
+struct RemoteImageView: View {
+    let imageURL: URL
+    var body: some View {
+        AsyncImage(url: imageURL) { image in
+            image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+        } placeholder: {
+            ProgressView() // Shows a loading indicator while the image is being fetched
+        }
+    }
+}
+
+extension VideosView {
+    class ViewModel: ObservableObject {
+        let title: String
+        let items: [VideoItem]
+        let backgroundColor: Color
+        init(
+            title: String,
+            items: [VideoItem],
+            backgroundColor: Color
+        ) {
+            self.title = title
+            self.items = items
+            self.backgroundColor = backgroundColor
+        }
+    }
+}
+
+#Preview {
+    VideosView(
+        viewModel: .init(
+            title: "Physiotherapy",
+            items: Dummy.physiotherapyVideoItems,
+            backgroundColor: Color(red: 75/255.0, green: 125/255.0, blue: 116/255.0)
+        )
+    )
+}
+
+extension Dummy {
+    static let physiotherapyVideoItems: [VideoItem] = {
+        [
+            .init(title: "Video 1", description: "Stormi is a dog. She is dark grey and has long legs. Her eyes are expressive and are able to let her humans know what she is thinking. Her tongue is long, pink, and wet. Her long legs allow her to sprint after other dogs, people or bunnies. She can be a good dog, but also very bad. Her tail wags when happy or excited and hides between her back legs when she is bad. Stormi is a dog I love.", url: "https://harvesttech.com.au/vids/vid.mp4", thumbnailURL: "https://harvesttech.com.au/vids/w1v3-t.jpg"),
+            .init(title: "Video 2", description: "It was a weird concept. Why would I really need to generate a random paragraph? Could I actually learn something from doing so? All these questions were running through her head as she pressed the generate button. To her surprise, she found what she least expected to see.", url: "https://harvesttech.com.au/vids/vid.mp4", thumbnailURL: "https://harvesttech.com.au/vids/w1v4-t.jpg"),
+            .init(title: "Video 3", description: "The young man wanted a role model. He looked long and hard in his youth, but that role model never materialized. His only choice was to embrace all the people in his life he didn't want to be like.", url: "https://harvesttech.com.au/vids/vid.mp4", thumbnailURL: "https://harvesttech.com.au/vids/w1v5-t.jpg"),
+            .init(title: "Video 4", description: "Bryan had made peace with himself and felt comfortable with the choices he made. This had made all the difference in the world. Being alone no longer bothered him and this was essential since there was a good chance he might spend the rest of his life alone in a cell.", url: "https://harvesttech.com.au/vids/vid.mp4", thumbnailURL: "https://harvesttech.com.au/vids/w1v6-t.jpg"),
+            .init(title: "Video 5", description: "The lone lamp post of the one-street town flickered, not quite dead but definitely on its way out. Suitcase by her side, she paid no heed to the light, the street or the town. A car was coming down the street and with her arm outstretched and thumb in the air, she had a plan.", url: "https://harvesttech.com.au/vids/vid.mp4", thumbnailURL: "https://harvesttech.com.au/vids/w2v1-t.jpg"),
+        ]
+    }()
+    static let occupationalTherapyVideoItems: [VideoItem] = { physiotherapyVideoItems }()
+    static let dieteticsVideoItems: [VideoItem] = { physiotherapyVideoItems }()
+    static let speechPathologyVideoItems: [VideoItem] = { physiotherapyVideoItems }()
+    static let fallsPreventionVideoItems: [VideoItem] = { physiotherapyVideoItems }()
+    static let homeSafetyAuditVideoItems: [VideoItem] = { physiotherapyVideoItems }()
+    static let telehealthHowToVideoItems: [VideoItem] = { physiotherapyVideoItems }()
+}
